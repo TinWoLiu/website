8b15dc4358c880579259f6f1e8aa3f0923badf03
Timmy Liu <timmyliu123321@gmail.com>
2023-11-02 22:57:23 +0000

    install modules

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import React, { useEffect } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { FiSettings } from 'react-icons/fi';
-import { TooltipCompenent } from '@syncfusion/ej2-react-popups';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 import './App.css'
 
 const App = () => {
