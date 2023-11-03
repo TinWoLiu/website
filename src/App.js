9013059b830a1aca46ed380e648ed62cee983a5d
TinWoLiu <timmyliu123321@gmail.com>
2023-11-03 22:18:35 +0000

    added hello world

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // this is the same as "main" in java
 import React, { useEffect } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; // all the 
 import { FiSettings } from 'react-icons/fi';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 import './App.css'
@@ -8,11 +8,12 @@ import './App.css'
 const App = () => {
   return (
     <div>
+        hello world!
         <BrowserRouter>
             <div className="flex relative dark:bg-main-dark-bg">  // for darkmode background
                 <div className="fixed right-4 bottom-4" style={{ zIndex: '1000' }}>
-                    <TooltipCompenent content="settings" position="Top">
-                        <button>
+                    <TooltipCompenent content="Settings" position="Top">
+                        <button type="button" className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white">
                             <FiSettings />
                         </button>
                     </TooltipCompenent>
